567865962248abfb498a1adba616dc2786d0eb01
awesomegame <alexander.taver@gmail.com>
2022-03-06 12:31:55 +0200

    3 files modified

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { DataService } from '../data.service';
 
 @Component({
@@ -12,56 +11,14 @@ export class ClientsComponent implements OnInit {
   clientForm = this.formBuilder.group({
     name: '',
   });
-  clients = [];
-  constructor(
-    private formBuilder: FormBuilder,
-    private http: HttpClient,
-    private data: DataService
-  ) {}
+  constructor(private formBuilder: FormBuilder, public data: DataService) {}
 
-  ngOnInit() {
-    this.clients = this.data.getClients();
-  }
+  ngOnInit() {}
   onSubmit(): void {
-    const headers = { 'content-type': 'application/json' };
-    var id = 1;
-    if (this.clients.length > 0)
-      id =
-        this.clients
-          .map((it) => parseInt(it.id))
-          .reduce((a, b) => (a > b ? a : b)) + 1;
-
-    const body = JSON.stringify({
-      id: id.toString(),
-      name: this.clientForm.value.name,
+    this.data.addClient(this.clientForm.value, () => {
+      this.clientForm = this.formBuilder.group({
+        name: '',
+      });
     });
-
-    this.http
-      .post(
-        'https://8edsojoa99.execute-api.us-east-1.amazonaws.com/prod/addOwner',
-        body,
-        { headers: headers }
-      )
-      .subscribe((data) => {
-        this.load();
-        this.clientForm = this.formBuilder.group({
-          name: '',
-        });
-      });
-  }
-
-  load(): void {
-    const headers = { 'content-type': 'application/json' };
-    const body = '{}';
-
-    this.http
-      .post(
-        'https://8edsojoa99.execute-api.us-east-1.amazonaws.com/prod/getOwners',
-        body,
-        { headers: headers, observe: 'body' }
-      )
-      .subscribe((data) => {
-        this.clients = JSON.parse(data['body']);
-      });
   }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,25 +3,50 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DataService {
-  private _clients = [];
+  clients;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.getClients();
+  }
 
   getClients() {
-    if (this._clients.length == 0) {
-      const headers = { 'content-type': 'application/json' };
-      const body = '{}';
+    const headers = { 'content-type': 'application/json' };
+    const body = '{}';
 
-      this.http
-        .post(
-          'https://8edsojoa99.execute-api.us-east-1.amazonaws.com/prod/getOwners',
-          body,
-          { headers: headers, observe: 'body' }
-        )
-        .subscribe((data) => {
-          this._clients = JSON.parse(data['body']);
-        });
-    }
-    return this._clients;
+    this.http
+      .post(
+        'https://8edsojoa99.execute-api.us-east-1.amazonaws.com/prod/getOwners',
+        body,
+        { headers: headers, observe: 'body' }
+      )
+      .subscribe((data) => {
+        this.clients = JSON.parse(data['body']);
+      });
+  }
+
+  addClient(client, cb) {
+    const headers = { 'content-type': 'application/json' };
+    var id = 1;
+    if (this.clients.length > 0)
+      id =
+        this.clients
+          .map((it) => parseInt(it.id))
+          .reduce((a, b) => (a > b ? a : b)) + 1;
+
+    const body = JSON.stringify({
+      id: id.toString(),
+      name: client.name,
+    });
+
+    this.http
+      .post(
+        'https://8edsojoa99.execute-api.us-east-1.amazonaws.com/prod/addOwner',
+        body,
+        { headers: headers }
+      )
+      .subscribe((data) => {
+        this.clients.push(JSON.parse(data['body']));
+        cb();
+      });
   }
 }

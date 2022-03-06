f94b4313995d0fc65d7863c626fc0d3d6a39af51
awesomegame <alexander.taver@gmail.com>
2022-03-06 11:07:24 +0200

    1 file modified, 1 added

--- src/app/clients-details/clients-details.component.ts
+++ src/app/clients-details/clients-details.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-clients-details',
   templateUrl: './clients-details.component.html',
-  styleUrls: ['./clients-details.component.css']
+  styleUrls: ['./clients-details.component.css'],
 })
 export class ClientsDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  client = { id: 0, name: '', ships: [] };
+  shipForm = this.formBuilder.group({
+    name: '',
+    dock: '',
+    size: '',
+    description: '',
+  });
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.route.params.subscribe((param) => {
+      this.client.id = param.clientId;
+    });
   }
 
+  ngOnInit() {}
+  onSubmit() {}
 }
